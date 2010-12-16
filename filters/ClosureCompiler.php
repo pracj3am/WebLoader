9a4ada2803a18d5e8df8f423a1b0b2fe9bcba95a
Jan Prachař <jan.prachar@gmail.com>
2010-12-16 04:03:48 +0100

    Closure Compiler filter: Handle HTTP error returned by service API.

--- filters/ClosureCompiler.php
+++ filters/ClosureCompiler.php
@@ -18,6 +18,7 @@ class ClosureCompiler
         \curl_setopt($ch, \CURLOPT_URL, 'http://closure-compiler.appspot.com/compile');
         \curl_setopt($ch, \CURLOPT_POST, 1);
         \curl_setopt($ch, \CURLOPT_RETURNTRANSFER, 1);
+        \curl_setopt($ch, \CURLOPT_FAILONERROR, 1);
         \curl_setopt($ch, \CURLOPT_POSTFIELDS, \http_build_query(array(
             'js_code' => $code,
             'compilation_level' => $this->compilationLevel,
